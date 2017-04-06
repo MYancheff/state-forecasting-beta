bca12406898cdc3ad3fb8201c293c519c9c9ff83
Ben Black <weepingwillowben@gmail.com>
2017-04-05 20:26:41 -0700

    restructured data to match afs repository

--- load_precinct_data.R
+++ load_precinct_data.R
@@ -2,16 +2,30 @@
 library(tidyverse)
 library(data.table)
 
+folder = "state-legislative-data/Precinct Level Election Results/"
 
 file_end = "(CSV Format).csv"
 statewide_filenames = paste((2004:2012)[c(TRUE, FALSE)],"Statewide General Election",file_end)
 gen_filenames = paste(c(2014,2016),"General Election Results",file_end)
 all_filenames = c(statewide_filenames,gen_filenames)
-all_paths = paste("data/",all_filenames,sep="")
+all_paths = paste(folder,all_filenames,sep="")
 
 load_file = function(filename){
   read_csv(filename,skip=3,na=c("","NA","*"))
 }
 
 files = lapply(all_paths,load_file)
-data = rbindlist(files)
+prec_data = rbindlist(files)
+
+
+##############################################
+# Road data
+##############################################
+
+road_filenames = paste(folder,"Nevada Precinct Results ",(1984:1990)[c(TRUE, FALSE)],".tsv",sep="")
+
+load_road = function(filename){
+  read_tsv(filename)
+}
+
+road_files = lapply(road_filenames,load_road)
