d90bd595b4a8c46fbe78cf956034a4fd60bf5540
Ben Black <weepingwillowben@gmail.com>
2017-04-08 16:00:05 -0700

    updated lstm, plots

--- load_econ_data.R
+++ load_econ_data.R
@@ -1,4 +1,1 @@
-folder = "state-legislative-data/Economic Data/"
-filename = "Nevada Annual Real Median Househod Income Data 1984-2017 BEA code MEHOINUSNVA672N.csv"
-path = paste(folder, filename,sep="")
-data = read_csv(path)
+
